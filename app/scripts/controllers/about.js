ba77d3dd5feaebdfb44d48314fc3c050ff017f4d
rbkh <rebekah@bigroomstudios.com>
2017-07-17 16:34:55 -0400

    added angular bootstrap with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularWireframingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularWireframingApp
+ */
+angular.module('angularWireframingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularWireframingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularWireframingApp
+ */
+angular.module('angularWireframingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
