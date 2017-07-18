3d7b231f22b5fbb824fab5385181b2f3277d069d
rbkh <rebekah@bigroomstudios.com>
2017-07-17 22:15:03 -0400

    adding more sample data, adding orderby to medication table

--- app/scripts/controllers/medication-list.js
+++ app/scripts/controllers/medication-list.js
@@ -14,19 +14,64 @@ angular.module('angularWireframingApp')
 
     $scope.medications = [
       {
-        name: 'advil',
+        name: 'Ibuprofen',
+        category: 'Tablet',
         quantity: 72,
         maxQuantity:  150
       },
       {
-        name: 'tylenol',
+        name: 'Acetaminophen',
+        category: 'Tablet',
         quantity: 17,
         maxQuantity:  200
       },
       {
-        name: 'insulin',
-        quantity: 225,
-        maxQuantity:  400
+        name: 'Lysine',
+        category: 'Compound Liquid',
+        quantity: '100ml',
+        maxQuantity:  '350ml'
+      },
+      {
+        name: 'Neomycin',
+        category: 'Drops',
+        quantity: '4.25ml',
+        maxQuantity:  '5ml'
+      },
+      {
+        name: 'Ciprofloxacin',
+        category: 'Ointment',
+        quantity: '2ml',
+        maxQuantity:  '5ml'
+      },
+      {
+        name: 'Amoxicillin',
+        category: 'Compound Liquid',
+        quantity: '13ml',
+        maxQuantity:  '15ml'
+      },
+      {
+        name: 'Methimazole',
+        category: 'Tablet',
+        quantity: '75',
+        maxQuantity:  '75'
+      },
+      {
+        name: 'Hydrocodone',
+        category: 'Tablet',
+        quantity: '24',
+        maxQuantity:  '25'
+      },
+      {
+        name: 'Amlodipine',
+        category: 'Compound Liquid',
+        quantity: '9.5ml',
+        maxQuantity:  '10ml'
+      },
+      {
+        name: 'Insulin',
+        category: 'Injectable',
+        quantity: '225u',
+        maxQuantity:  '1000u'
       }
     ];
   });
